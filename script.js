f42366d75b541e8489a4adb04b08a4a5eeb3d6ab
duarte <matheus240377@gmail.com>
2025-06-06 17:24:07 -0300

    alterando numero

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let numeroJoao = 5511955303539
-let numeroCarlos = 5511972541477
-
 function abrirWhatsApp() {
     const telefone = '5511955303539'; // Substitua pelo número da barbearia
     const mensagem = 'Olá! Gostaria de agendar um horário na Barbearia Estilo Fino.';
